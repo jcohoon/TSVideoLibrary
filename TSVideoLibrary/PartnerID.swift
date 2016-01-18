98b29d4a34b196aa913a60f07b08a9ff174d028d
JJ Cohoon <james.cohoon@teachstone.com>
2016-01-18 13:03:48 -0500

    testing xcode gitignore file

---
+++ TSVideoLibrary/PartnerID.swift
@@ -0,0 +1,11 @@
+//
+//  PartnerID.swift
+//  TSVideoLibrary
+//
+//  Created by James Cohoon on 1/18/16.
+//  Copyright © 2016 Alliterative Software. All rights reserved.
+//
+
+import Foundation
+
+let partnerID = 123456
--- TSVideoLibrary/kaltura.swift
+++ TSVideoLibrary/kaltura.swift
@@ -11,22 +11,20 @@ import Foundation
 /*
 class CodeExample{
     static void Main(string[] args){
-    KalturaConfiguration config = new KalturaConfiguration(partnerId);
-    config.ServiceUrl = "https://www.kaltura.com/";
-    KalturaClient client = new KalturaClient(config);
-    String secret = null;
-    String userId = null;
-    KalturaSessionType type = null;
-    int partnerId = null;
-    int expiry = null;
-    String privileges = null;
-    Object result = client.SessionService.Start(secret, userId, type, partnerId, expiry, privileges);
+        KalturaConfiguration config = new KalturaConfiguration(partnerId);
+        config.ServiceUrl = "https://www.kaltura.com/";
+        KalturaClient client = new KalturaClient(config);
+        String secret = null;
+        String userId = null;
+        KalturaSessionType type = null;
+        int partnerId = null;
+        int expiry = null;
+        String privileges = null;
+        Object result = client.SessionService.Start(secret, userId, type, partnerId, expiry, privileges);
     }
 }
-
+*/
 
 class KSessionSettings {
     let partnerID 
 }
-
-*/
